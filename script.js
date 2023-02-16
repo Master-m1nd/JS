f07785f8f1fb76c99ed4e0faa49e1fe2e3f2a35c
Dean_Lukachynets <lukachynetsdean@gmail.com>
2023-02-17 01:24:59 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log (`You can ignore single and double quotes like this: \\ '\\' \\" \\"`)
+
+
+alert('Please enter your data');
+let userName = prompt("What's your name?", 'Name');
+alert(`Your name is: ${userName}`)
+let surName = prompt("Enter your surname?", 'Surname');
+alert(`Your full name is: ${userName} ${surName}`)
+let age= prompt("How old are you?", 'Age');
+alert(`Your full name is: ${userName} ${surName}, your age is: ${age}`)
+let admin = confirm('Are you admin?');
+alert(`Your full name is: ${userName} ${surName}, your age is: ${age}, admin status: ${admin}`);
+
+
