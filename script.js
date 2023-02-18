12b91b8f4217925490901db2e42b643834c449e9
Dean_Lukachynets <lukachynetsdean@gmail.com>
2023-02-18 19:17:31 +0200

    homework 18 done

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+            // Home work 17
 console.log (`You can ignore single and double quotes like this: \\ '\\' \\" \\"`)
 
 
@@ -11,4 +12,21 @@ alert(`Your full name is: ${userName} ${surName}, your age is: ${age}`)
 let admin = confirm('Are you admin?');
 alert(`Your full name is: ${userName} ${surName}, your age is: ${age}, admin status: ${admin}`);
 
+            //Home work 18
 
+let firstMonthSalary = prompt("Enter first month salary?", '1');
+
+let secondMonthSalary = prompt("Enter second month salary?", '2');
+
+let total = Number(firstMonthSalary) + Number(secondMonthSalary);
+
+alert(`Your salary for 2 month is: ${total}$`);
+
+let totalPlus = total + 1;
+
+alert(`Your have an extra bonus! Your total salary was: ${total}$ It's increase for 1$ and now it is: ${totalPlus}$`);
+
+let minSalary = 2000;
+
+if (totalPlus < minSalary) alert('I`m out')
+if (totalPlus >= minSalary) alert('I`m ready to work')
