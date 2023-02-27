4b284f68272be9c358c435027547c0d2f7c25157
Dean_Lukachynets <lukachynetsdean@gmail.com>
2023-02-27 14:13:52 +0200

    hw 19 done

--- script.js
+++ script.js
@@ -1,18 +1,67 @@
-            // Home work 17
-console.log (`You can ignore single and double quotes like this: \\ '\\' \\" \\"`)
+        //Home work 19
 
+//1
 
-alert('Please enter your data');
-let userName = prompt("What's your name?", 'Name');
-alert(`Your name is: ${userName}`)
-let surName = prompt("Enter your surname?", 'Surname');
-alert(`Your full name is: ${userName} ${surName}`)
-let age= prompt("How old are you?", 'Age');
-alert(`Your full name is: ${userName} ${surName}, your age is: ${age}`)
-let admin = confirm('Are you admin?');
-alert(`Your full name is: ${userName} ${surName}, your age is: ${age}, admin status: ${admin}`);
+let personAge = prompt("How old are you?", 'Age');
 
-            //Home work 18
+if(personAge <= 17) {
+    confirm('Do you stady at school?')
+}
+else if(personAge > 17 && personAge < 25) {
+    confirm('Do you study at university?')
+}
+else if(personAge > 25 && personAge < 60) {
+    confirm('Do you have a job?')
+}
+else {
+    confirm('What are you doing?')
+}
+
+//2
+
+let displayProperty = prompt('Guess the display property', 'Property');
+
+switch(displayProperty){
+    case 'block':
+        alert('display:block');
+        break;
+    case 'flex':
+        alert('display:flex');
+        break;
+    case 'grid':
+        alert('display:grid' );
+        break;
+    case 'inline':
+        alert('display:inline' );
+        break;
+    case 'none':
+        alert('display:none');
+        break;
+    default:
+        alert("Set 'display: inline-block;'");
+ }
+
+ //3
+
+let numbers = prompt('Please enter the number', '1');
+
+while(true) {
+    alert(`${numbers} by 12 equals ${+numbers*12}`) 
+    if (numbers >= 0) break;
+    numbers = prompt('Your number is incorect please enter correct number', '1') 
+    if(numbers >= 0 || numbers < 0 && +numbers == NaN);
+}
+
+ //4
+
+ for (let i = 2; i < 50; i++) {
+    if (i % 2 == 0) {
+        console.log(i)
+    }
+ }        
+     
+     
+     //Home work 18
 
 let firstMonthSalary = prompt("Enter first month salary?", '1');
 
@@ -30,3 +79,20 @@ let minSalary = 2000;
 
 if (totalPlus < minSalary) alert('I`m out')
 if (totalPlus >= minSalary) alert('I`m ready to work')
+
+
+
+        // Home work 17
+
+console.log (`You can ignore single and double quotes like this: \\ '\\' \\" \\"`)
+
+
+alert('Please enter your data');
+let userName = prompt("What's your name?", 'Name');
+alert(`Your name is: ${userName}`)
+let surName = prompt("Enter your surname?", 'Surname');
+alert(`Your full name is: ${userName} ${surName}`)
+let age = prompt("How old are you?", 'Age');
+alert(`Your full name is: ${userName} ${surName}, your age is: ${age}`)
+let admin = confirm('Are you admin?');
+alert(`Your full name is: ${userName} ${surName}, your age is: ${age}, admin status: ${admin}`);
