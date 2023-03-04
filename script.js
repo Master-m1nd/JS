ca0169839ea9aeee5892a2202b3832a0cc879453
Dean_Lukachynets <lukachynetsdean@gmail.com>
2023-03-04 14:06:56 +0200

    home work 23 done

--- script.js
+++ script.js
@@ -1,4 +1,73 @@
-       //Home work 22
+'use strict'
+
+        //Home work 23
+
+//1
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let sum = (arr) => (arr.length === 0) ? 0 : arr[0] + sum(arr.slice(1));
+
+console.log(sum(arr));
+
+//2
+
+const deepCount = (arr = []) => {
+    return arr
+    .reduce((acc, val) => {
+       return acc + (Array.isArray(val) ? deepCount(val) : 0);
+    }, arr.length);
+ };
+ console.log(deepCount([1, 2, [3]]));
+
+//3
+
+let employees = {
+    development: {
+        backend: [{name: 'Mike', salary: 2000}, {name: 'Nikola', salary: 2500}],
+        fronted: [{name: 'Artem', salary: 3000}, {name: 'Alex', salary: 2700}],
+    },
+    sales: {
+        marketing: {
+            internetMarketers: [{name: 'Nina', salary: 1000}, {name: 'Olena', salary: 1300}],
+            promotion: [{name: 'Oleg', salary: 1400}, {name: 'Masha', salary: 1700}],
+        },
+        sellers: [{name: 'Max', salary: 900}, {name: 'Donald', salary: 700}, {name: 'Joe', salary: 1100}]
+    },
+    designer: [{name: 'Kate', salary: 1800}]
+}
+
+function sumArraySalaries() {
+    let sum = 0;
+    return function sumSallary(obj) {
+        for(let key in obj) {
+           if (Array.isArray(obj[key])) {
+             obj[key].forEach(employees => sum += employees.salary);
+            }
+            else {
+             sumSallary(obj[key]);
+            }
+        }
+        console.log(sum);
+        return sum;
+    }   
+}   
+
+console.log(sumArraySalaries()(employees));
+
+//4
+
+let arr1 = [2, 15, 7, 3];
+
+let arr2 = [9, 3, 17, 12, 4, 8];
+
+let arr3 = [6, 11, 16, 15, 11];
+
+let countArray = [...arr1, ...arr2, ...arr3];
+
+console.log(Math.max(...countArray));
+
+        //Home work 22
        
 //1
 
